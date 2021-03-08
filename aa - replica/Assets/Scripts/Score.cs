1a19a9d820e22ad7f1efe78eefab658c5fd77a9d
princeus93 <69991982+princeus93@users.noreply.github.com>
2021-03-07 19:02:09 -0600

    Finished Brackeys tutorial

---
+++ aa - replica/Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int PinCount = 0;
+    public Text text;
+
+    private void Start()
+    {
+        PinCount = 0;
+    }
+    private void Update()
+    {
+        text.text = PinCount.ToString();
+    }
+}
